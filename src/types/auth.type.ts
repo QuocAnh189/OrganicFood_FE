ae518fd336f43e0a1da2e047da44bf722b9499f7
QuocAnh189 <anhquoc18092003@gmail.com>
2024-01-26 22:10:56 +0700

    add Redux

---
+++ src/types/auth.type.ts
@@ -0,0 +1,14 @@
+export type SignInType = {
+  email: string;
+  password: string;
+};
+
+export type SignUpType = {
+  email: string;
+  firstname: string;
+  lastname: string;
+  fullname: string;
+  phone: string;
+  password: string;
+  confirmpassword: string;
+};
