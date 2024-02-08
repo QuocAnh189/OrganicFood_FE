8301aa69f5833de9893dc3ff3f6db62902efa494
QuocAnh189 <anhquoc18092003@gmail.com>
2024-02-08 17:24:24 +0700

    update Auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    API_URL: process.env.API_URL
+  },
+  webpack(config) {
+    return config;
+  }
+};
 
 export default nextConfig;
--- src/app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { CircularProgress } from '@chakra-ui/react';
-
-export default function Loading() {
-  return <CircularProgress color="#333333" />;
-}
